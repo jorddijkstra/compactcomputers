b59a7f1af8c684210a5fe84207f9de66f5d4e0e7
Yochem van Rosmalen <yochem@icloud.com>
2019-04-05 21:04:41 +0200

    Show menu on about page

---
+++ js/about.js
@@ -0,0 +1,24 @@
+function createMenu(data) {
+
+    var categorySet = [];
+    $('body').append('<ul id="menu">HOME</ul>');
+
+    for (var entry in data) {
+        var category = data[entry].category;
+
+        // only add to the set if it's not there yet
+        if (!categorySet.includes(category)) {
+            categorySet.push(category);
+            var formattedCategory = category.replace(/-/g, ' ');
+            $('#menu').append(`<li><a href="gallery.html?c=${category}">
+                ${formattedCategory}</a></li>`);
+        }
+    }
+}
+
+$.getJSON("data/data.json", function(data) {
+    createMenu(data);
+}).fail(function(err) {
+    alert('error loading data');
+    console.log(err);
+});
